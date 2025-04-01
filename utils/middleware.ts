5fbc3690b7ab251c0c02d262b099a99916b7064b
trufurs <rajsinghjain.jp@gmail.com>
2025-04-01 13:29:26 +0530

    middleware

--- utils/middleware.ts
+++ utils/middleware.ts
@@ -1,17 +1,17 @@
 // Code for middleware function that checks if user is authenticated
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//     const token = req.cookies.get("token")?.value;
+export function middleware(req: NextRequest) {
+    const token = localStorage.getItem("auth_token");
 
-//     if (!token) {
-//         return NextResponse.redirect(new URL("/login", req.url));
-//     }
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
 
-//     return NextResponse.next();
-// }
+    return NextResponse.next();
+}
 
-// // Apply middleware only to protected routes
-// export const config = {
-//     matcher: ["/inventory", "/map", "/orders"],
-// };
+// Apply middleware only to protected routes
+export const config = {
+    matcher: ["/inventory", "/map", "/orders"],
+};
